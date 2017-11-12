c06c58839d7accceaecddaba8c91df0d5a1daeb3
Richard Tabor <hello@themebeans.com>
2017-11-11 21:47:37 -0500

    Removed updates

--- bean-portfolio.php
+++ bean-portfolio.php
@@ -18,107 +18,11 @@
 /*===================================================================*/
 /* MAKE SURE WE DO NOT EXPOSE ANY INFO IF CALLED DIRECTLY
 /*===================================================================*/
-if ( !function_exists( 'add_action' ) ) 
+if ( !function_exists( 'add_action' ) )
 {
     echo 'Hi there!  I\'m just a plugin, not much I can do when called directly.';
     exit;
 }
-
-
-
-
-/*===================================================================*/
-/*
-/* PLUGIN FEATURES SETUP
-/*
-/*===================================================================*/
-$bean_plugin_features[ plugin_basename( __FILE__ ) ] = array(
-     "updates" => false // CHANGE THIS TO FALSE TO TURN OFF UPDATES
-);
-
-if ( ! function_exists( 'bean_plugin_supports' ) ) 
-{
-	function bean_plugin_supports( $plugin_basename, $feature ) 
-	{
-		global $bean_plugin_features;
-
-		$setup = $bean_plugin_features;
-
-		if( isset( $setup[$plugin_basename][$feature] ) && $setup[$plugin_basename][$feature] )
-			return true;
-		else
-			return false;
-	}
-}
-
-
-
-
-/*===================================================================*/
-/*
-/* PLUGIN UPDATER FUNCTIONALITY
-/*
-/*===================================================================*/
-define( 'EDD_BEANPORTFOLIO_TB_URL', 'http://themebeans.com' );
-define( 'EDD_BEANPORTFOLIO_NAME', 'Bean Portfolio' );
-
-if ( bean_plugin_supports ( plugin_basename( __FILE__ ), 'updates' ) ) : // check to see if updates are allowed; only import if so
-
-//LOAD UPDATER CLASS
-if( !class_exists( 'EDD_SL_Plugin_Updater' ) ) 
-{
-	include( dirname( __FILE__ ) . '/updates/EDD_SL_Plugin_Updater.php' );
-}
-//INCLUDE UPDATER SETUP
-include( dirname( __FILE__ ) . '/updates/EDD_SL_Activation.php' );
-
-
-endif; // END if ( bean_plugin_supports ( plugin_basename( __FILE__ ), 'updates' ) )
-
-
-/*===================================================================*/
-/* UPDATER SETUP
-/*===================================================================*/
-function beanportfolio_license_setup() 
-{
-	add_option( 'edd_beanportfolio_activate_license', 'BEANPORTFOLIO' );
-	add_option( 'edd_beanportfolio_license_status' );
-}
-add_action( 'init', 'beanportfolio_license_setup' );
-
-function edd_beanportfolio_plugin_updater() 
-{
-    // check to see if updates are allowed; don't do anything if not
-    if ( ! bean_plugin_supports ( plugin_basename( __FILE__ ), 'updates' ) ) return;
-
-	//RETRIEVE LICENSE KEY
-	$license_key = trim( get_option( 'edd_beanportfolio_activate_license' ) );
-
-	$edd_updater = new EDD_SL_Plugin_Updater( EDD_BEANPORTFOLIO_TB_URL, __FILE__, array( 
-			'version' 	=> '1.5',
-			'license' 	=> $license_key,
-			'item_name'    => EDD_BEANPORTFOLIO_NAME,
-			'author' 	     => 'ThemeBeans'
-		)
-	);
-}
-add_action( 'admin_init', 'edd_beanportfolio_plugin_updater' );
-
-
-/*===================================================================*/
-/* DEACTIVATION HOOK - REMOVE OPTION
-/*===================================================================*/
-function beanportfolio_deactivate() 
-{
-	delete_option( 'edd_beanportfolio_activate_license' );
-	delete_option( 'edd_beanportfolio_license_status' );
-}
-register_deactivation_hook( __FILE__, 'beanportfolio_deactivate' );
-
-
-
-
-
 
 
 
